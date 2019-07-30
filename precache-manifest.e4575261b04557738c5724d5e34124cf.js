3c1b8154d954427dfd1946692265e9d91f9c2ae4
alex <earthhuman41@gmail.com>
2019-07-30 15:34:16 +0300

    Updates

---
+++ precache-manifest.e4575261b04557738c5724d5e34124cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e23de85ae4932da99b8dc42dafb61cb",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "40e0888d3f02da3accc0",
+    "url": "/react_pagination/static/css/main.1000019f.chunk.css"
+  },
+  {
+    "revision": "5209d4948457e0e1787c",
+    "url": "/react_pagination/static/js/2.dce615b3.chunk.js"
+  },
+  {
+    "revision": "40e0888d3f02da3accc0",
+    "url": "/react_pagination/static/js/main.28b1c42f.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
